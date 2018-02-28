4f3054cef12d262ec35b5366245d553c9a80f45a
emreerkoca <emreerkoca@outlook.com>
2018-02-28 12:24:17 +0300

    Add project files.

---
+++ ProductProject/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ProductProject.Startup))]
+namespace ProductProject
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
