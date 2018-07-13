007151cdad54f9ef1a6f1164a41af9da3488a823
Emre Erkoca <emreerkoca@outlook.com>
2018-07-13 21:22:51 +0300

    Building Web Api

---
+++ ProductProject/App_Start/MappingProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using ProductProject.Dtos;
+using ProductProject.Models;
+
+namespace ProductProject
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            Mapper.CreateMap<Customer, CustomerDto>();
+            Mapper.CreateMap<CustomerDto, Customer>();
+        }
+    }
+}
